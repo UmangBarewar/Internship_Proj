2cd0ebbc7d2ae4e72d802270f3c06e442c6732e7
Umang Barewar <umangbarewar28@gmail.com>
2025-04-12 01:21:20 +0530

    fixed the path according to assets folder

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(frontendPath));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
+    res.sendFile(path.join(frontendPath, "assets", "index.html"));
   });
 }
 
