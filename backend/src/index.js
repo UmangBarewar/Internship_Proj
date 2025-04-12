b994129555e6d5fdd2a93bfb65872392baaa50eb
Umang Barewar <umangbarewar28@gmail.com>
2025-04-12 11:06:44 +0530

    old school

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,11 +31,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../../frontend/dist");
-  app.use(express.static(frontendPath));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "assets", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
